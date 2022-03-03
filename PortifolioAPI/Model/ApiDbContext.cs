88ab7eb4d1262ad97c34f35726703bdea9c561fc
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2022-03-02 21:26:21 -0300

    Add project files.

---
+++ PortifolioAPI/Model/ApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortifolioAPI.Model;
+
+namespace ApiSimples.Model
+{
+    public class ApiDbContext: DbContext
+    {
+        public ApiDbContext(DbContextOptions options): base(options)   
+        { 
+        
+        
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+
+    }
+}
